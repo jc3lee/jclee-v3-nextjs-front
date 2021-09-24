6fb67000df086297b736243a6678a077f2b5631b
jclee <dev.if.ljc@gmail.com>
2021-09-24 16:37:20 +0200

    updated head meta

--- components/MyHead.tsx
+++ components/MyHead.tsx
@@ -1,10 +1,13 @@
 import Head from 'next/head'
+import { myWebsite } from '../myData/myHeadConfig'
 
 type Props = {
-  title?: string
+  title: string
   description: string
   imgSrc: string
 }
+
+// Meta Tags Generated via http://heymeta.com 
 
 const MyHead = ({ description, title, imgSrc }: Props) => {
   return (
@@ -12,16 +15,27 @@ const MyHead = ({ description, title, imgSrc }: Props) => {
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+      {/* <!-- HTML Meta Tags --> */}
+      <meta name="description" content={description} />
+
+      {/* <!-- Google / Search Engine Tags --> */}
+      <meta itemProp="name" content={title} />
+      <meta itemProp="description" content={description} />
+      <meta itemProp="image" content={imgSrc} />
+
+      {/* <!-- Facebook Meta Tags --> */}
+      <meta property="og:url" content={myWebsite} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={imgSrc} />
+
+      {/* <!-- Twitter Meta Tags --> */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
-      <meta
-        name="twitter:description"
-        content={description}
-      />
-      <meta
-        name="twitter:image"
-        content={imgSrc}
-      />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={imgSrc} />
     </Head>
   )
 }
--- myData/myHeadConfig.ts
+++ myData/myHeadConfig.ts
@@ -1,4 +1,5 @@
-export const defaultTitle = "JC Lee | Web designer & developer"
-export const defaultCardSrc = "/images/card0.png"
+export const defaultTitle = "JC Lee | Web Designer & Developer"
+export const myWebsite = "https://jclee-v3.vercel.app"
+export const defaultCardSrc = `${myWebsite}/images/card0.png`
 export const defaultDescription = "I build the best experience for your users."
 export const blogHomeDescription = "I touch on Web development, Design and random insights from my journey into tech."
