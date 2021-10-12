a8a57399190c4801eb002998b0505c6d1d607766
jclee <dev.if.ljc@gmail.com>
2021-10-12 22:29:29 +0200

    design tweak

--- components/ecom/ItemDesc.tsx
+++ components/ecom/ItemDesc.tsx
@@ -27,8 +27,8 @@ const ItemDesc = ({ className, item, cart, setCart, handleAddedToCart }: Props) 
 
   return (
     <div className={className}>
-      <h1 className="font-bold text-3xl sm:leading-relaxed">{item.title}</h1>
-      <p className="text-lg text-blue-900 font-work font-bold">{displayPrice}</p>
+      <h1 className="font-bold text-2xl sm:text-3xl sm:leading-relaxed">{item.title}</h1>
+      <p className="text-lg sm:text-xl text-blue-900 font-work font-bold">{displayPrice}</p>
       <button onClick={handleAddToCartBtnClick} className="text-sm mt-8 uppercase tracking-wider w-96 py-3.5 max-w-full text-blue-900 border border-blue-800 rounded-sm font-bold focus:outline-none">Add to Cart</button>
       <button onClick={() => handleItemCheckout(item.itemId, item.pricing[0].priceId, 1)} className="text-sm mt-4 uppercase tracking-wider w-96 py-3.5 max-w-full bg-blue-900 hover:bg-opacity-90 transition-all duration-100 text-white rounded-sm font-bold focus:outline-none">Buy it Now</button>
       <p className="text-base mt-16">{item.description}</p>
